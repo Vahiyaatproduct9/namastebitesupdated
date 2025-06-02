cfb2bc7a5575b5742a51b7eb0e548c883c317ce7
unknown <kishordebnath123123@gmail.com>
2025-06-02 14:29:19 +0530

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,67 @@
 import logo from "../images/logo.png"
+import './Navbar.css';
+import menu from "../images/menu.png";
 export default function Navbar() {
+
+    let lastscroll = window.scrollY;
+    const bar = document.querySelector('.head-nav');
+
+    window.addEventListener('scroll', () => {
+
+        if (window.scrollY > lastscroll) {
+            bar.style.top = "-100px";
+        }
+        else {
+            bar.style.top = "0px";
+        }
+
+    })
+
+    function handleToggle() {
+        const bar = document.getElementById("bar");
+        const disp = getComputedStyle(bar).display;
+        disp === "none" ? bar.style.display = "block" : bar.style.display = "none";
+        const toggle = document.getElementById("toggle");
+        const toggleOn = getComputedStyle(toggle).display;
+
+        toggle.style.display = toggleOn === 'none' ?
+            'block' : 'none';
+
+        const strong = document.querySelector('strong');
+        strong.style.display = (toggleOn === 'block') ?
+            'block' : 'none';
+        const nav = document.querySelector('.head-nav');
+        nav.style.backgroundColor = (toggleOn === 'block') ?
+            'rgba(51, 51, 65, 1)' : 'transparent';
+
+    }
+
     return (
-        <>    <nav class="head-nav">
+        <>    <nav className="head-nav">
             <a href="https://vahiyaatproduct9.github.io/namastebites/">
-                <div class="brand">
+                <div className="brand">
                     <img src={logo} alt="Bites Logo Image" />
-                    <h3>Namaste Bites</h3>
+
+                    {/* <h3>Namaste Bites</h3> */}
                 </div>
             </a>
-            <ul id="bar" class="bar">
+            <div className="toggle" onClick={handleToggle}>
+                <img id="toggle" src={menu} alt="Menu" />
+
+                <strong style={{
+                    fontWeight: 800,
+                    fontSize: "4rem",
+                    display: "none"
+                }}>&times;</strong>
+            </div>
+            <ul id="bar" className="box">
                 <li id="home">Home</li>
                 <li id="aboutus">About Us
                     <ul>
                         <li>Owner's Words</li>
                     </ul>
                 </li>
-                <li class="menu">Menu
+                <li className="menu">Menu
                     <ul>
                         <li>Momo</li>
                         <li>Chowmien</li>
@@ -28,7 +74,7 @@ export default function Navbar() {
                 <li>Gallery</li>
                 <li>Hiring</li>
                 <li>Contact</li>
-                <li className="bookaseat">Book a Seat</li>
+                <li className="bookaseat"><span>Book a Seat</span></li>
             </ul>
         </nav>
         </>
